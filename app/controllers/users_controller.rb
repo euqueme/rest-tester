bc56c38c29da07c93c3436a0cf3fb2b17c2de2b4
Audrey-Ella-xo <anitaudrey@gmail.com>
2019-12-20 23:21:26 +0100

    Added the Users controller, the HTTP get methods and tested them with rest client

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    def index
+        @index_name = 'I am the index Action'
+    end
+
+    def show
+        @show_name = 'I am the show Action'
+    end
+
+    def new
+        @new_name = 'I am the new Action'
+    end
+
+    def edit
+        @edit_name = 'I am the Edit Action'
+    end
+end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,6 +1,7 @@
   # app/script/rest_requestor.rb
   require 'rest-client'
-  url = "http://localhost:3000"
+  
+  url = "http://localhost:3000/users"
 
   puts RestClient.get(url)
 

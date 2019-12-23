3743cce782ef8494a8b8d0815662b244f35f50f2
euqueme <euqueme@gmail.com>
2019-12-23 10:33:39 -0600

    MODIFY README AND ADD STICKLER FILES

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,12 +1,14 @@
-  # app/script/rest_requestor.rb
-  require 'rest-client'
-  
-  url = "http://localhost:3000/users"
+# frozen_string_literal: true
 
-  puts RestClient.get(url)
+# app/script/rest_requestor.rb
+require 'rest-client'
 
-  puts RestClient.get(url+'/new')
-  puts RestClient.get(url+'/edit')
-  puts RestClient.get(url+'/show')
-  
-  puts RestClient.post(url,'') #this is an error
+url = 'http://localhost:3000/users'
+
+puts RestClient.get(url)
+
+puts RestClient.get(url + '/new')
+puts RestClient.get(url + '/edit')
+puts RestClient.get(url + '/show')
+
+puts RestClient.post(url, '') # this is an error
